95d44609bb8c9fb8a7738b24d45b9a27e7e81a2f
Kishan Patel <patel.m.kishan@gmail.com>
2013-10-20 15:08:54 -0400

    Initialized AngularJS app.
    Essentially created the app structure. Still have to integrate the
    charting library.

---
+++ app/js/controllers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Controllers */
+
+var mabvControllers = angular.module('mabvControllers', []);
+
+mabvControllers.controller('FileDataCtrl', ['$scope'
+  function FileDataCtrl($scope) {
+  }); 
+]);
