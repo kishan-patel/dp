6e070189056ae48707cd531c7390fffff12e7157
Kishan Patel <patel.m.kishan@gmail.com>
2014-02-06 16:34:20 -0500

    Changed server port information.

--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ var https = require('https')
   
 var app = express();
 var httpServer = app.listen(config.APP_PORT);
+var httpServer = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 var io = require('socket.io').listen(httpServer);
 
 //App configurations
